97aa55621aab45f4c90e1573e55f7f39465dc79e
jonsnation <jonathanswamber2003@gmail.com>
2025-02-15 19:06:15 -0400

    - Implemented sidebar toggle functionality and adjust layout styles - Removed logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/navbar'
 import Sidebar from './components/sidebar/sidebar'
@@ -7,21 +7,28 @@ import Students from './views/Student'
 import './App.css'
 
 const App = () => {
+  const [isSidebarVisible, setSidebarVisible] = useState(true)
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible)
+  }
+
   return (
     <Router>
       <div className='container'>
-        <Navbar />
+        <Navbar toggleSidebar={toggleSidebar} />
         <div className='main-content'>
-          <Sidebar />
-          <Routes>
-            <Route path="/classroom" element={<Classroom />} />
-            <Route path="/students" element={<Students />} />
-          </Routes>
+          {isSidebarVisible && <Sidebar />}
+          <div className={isSidebarVisible ? 'content' : 'content full'}>
+            <Routes>
+              <Route path="/classroom" element={<Classroom />} />
+              <Route path="/students" element={<Students />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
   )
 }
-
 
 export default App
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,10 +3,10 @@ import './navbar.css'
 import logo_light from '../../assets/logo-black.png'
 import search_icon_light from '../../assets/search-w.png'
 
-const Navbar = () => {
+const Navbar = ({ toggleSidebar }) => {
   return (
     <div className='navbar'>
-      <img src={logo_light} alt="Light Logo" className='logo' />
+      <button className='toggle-button' onClick={toggleSidebar}>☰</button>
       <div className='header'>
         <h1 className='header-text'>Teacher Dashboard</h1>
       </div>
