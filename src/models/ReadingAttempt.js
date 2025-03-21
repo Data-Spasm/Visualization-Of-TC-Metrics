9e09f7fdfc09ccf10a8b8637f15e8236f3baeff8
Danara Sahadeo <danara.sahadeo@my.uwi.edu>
2025-03-21 01:16:18 -0400

    Add models for Avatar, ReadingAssessment, ReadingAttempt, Role, and User to interact with new data

---
+++ src/models/ReadingAttempt.js
@@ -0,0 +1,28 @@
+// src/models/ReadingAttemptModel.js
+// This file is used to interact with the reading assessment attempts data
+import { readingAssessmentAttempts } from '../utils/loadData';
+import { extractMisreadWords } from '../utils/misreadWordsProcessor';
+
+class ReadingAttempt {
+  static getAttemptById(id) {
+    return readingAssessmentAttempts.find(attempt => attempt._id.$oid === id);
+  }
+
+  static getAttemptsByStudent(username) {
+    return readingAssessmentAttempts.filter(attempt => attempt.studentUsername === username);
+  }
+
+  static getAttemptsByAssessment(assessmentId) {
+    return readingAssessmentAttempts.filter(attempt => attempt.readingAssessmentId === assessmentId);
+  }
+
+  static getAllAttempts() {
+    return readingAssessmentAttempts;
+  }
+
+  static getMisreadWords() {
+    return extractMisreadWords(readingAssessmentAttempts);
+  }
+}
+
+export default ReadingAttempt;
