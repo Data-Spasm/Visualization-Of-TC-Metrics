8c69777dca9b9d844b837202b738a68d4b4d045e
jonsnation <jonathanswamber2003@gmail.com>
2025-04-01 04:53:27 -0400

    - Added environment variable handling and remove sensitive data from the repository

--- server/index.cjs
+++ server/index.cjs
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config(); // Load .env variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,7 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://jonathanswamber2003:PR6aMw4u345v7fxT@cluster0.8rf33.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGO_URI;
+
+if (!uri) {
+  console.error("MONGO_URI is not set in environment variables.");
+  process.exit(1);
+}
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,6 +25,7 @@ const client = new MongoClient(uri, {
 });
 
 const dbName = "textcomplexitydb";
+
 
 async function main() {
   try {
